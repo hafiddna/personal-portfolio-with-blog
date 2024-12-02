23d2a3655f15fd76c90d72b4445634ed1051b7a4
Hafid Dian Nurfaujan Ahat <ambonxnobody.15072001@gmail.com>
2024-12-02 20:26:53 +0700

    Save progresses

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
+    // images: {
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'api.osingbahari.com',
+    //         },
+    //         {
+    //             protocol: 'http',
+    //             hostname: 'localhost',
+    //             port: '3010',
+    //             pathname: '/api/**',
+    //         }
+    //     ],
+    // }
 };
 
 export default nextConfig;
--- src/app/cms/contact/page.tsx
+++ src/app/cms/contact/page.tsx
@@ -1,4 +1,7 @@
-
+import { BiLogoDevTo } from "react-icons/bi";
+import { FaMediumM } from "react-icons/fa";
+import { FaGithub, FaXTwitter, FaInstagram, FaLinkedin, FaFacebook, FaHashnode, FaStackOverflow, FaCodepen, FaHackerrank, FaYoutube, FaTwitch, FaThreads, FaSquareBehance, FaDribbble } from "react-icons/fa6";
+import { SiCodesandbox, SiLeetcode, SiCodewars, SiTopcoder } from "react-icons/si";
 
 export default function CMSContact() {
     return (
